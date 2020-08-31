6fda47465db972798501877fed575e70af223a2e
Keng Lee <KengLee@Hoeungs-MacBook-Pro.local>
2020-08-31 15:39:51 -0700

    modals are displaying correctly now

--- SEIRfirstproject/app.js
+++ SEIRfirstproject/app.js
@@ -1,66 +1,129 @@
-
 $(() => {
 
 
 
-$.ajax({
-  url: 'https://swapi.dev/api/people',
-  type: 'GET'
-}).then(
-  (data) => {
-      $('#card1').html(data.results[0].name);
-      $('#card1').on('click', openModal)
-      $('#hair-color').text('hair color: ' + data.results[0].hair_color)
-      $('#eye-color').text('eye color: ' + data.results[0].eye_color)
-      $('#height').text('height: ' + data.results[0].height)
-      $('#card2').html(data.results[9].name);
-      $('#card7').html(data.results[3].name);
-  },
-)
+    let link = `https://swapi.dev/api/people`
 
-$.get("http://swapi.dev/api/people/?page=2", function(data) {
-  $('#card3').html(data.results[0].name);
-  $('#card4').html(data.results[8].name);
-  $('#card9').html(data.results[9].name);
-})
+  $.ajax({
+    url: link,
+    type: 'GET',
+  }).then(
+    (data) => {
+      $('#card2').html(data.results[9].name).on('click', display)
+      $('#card7').html(data.results[3].name).on('click', display)
+      $('#card1').html(data.results[0].name).on('click', display)
+      }
+      )
+      $.get("http://swapi.dev/api/people/?page=2", (data) => {
+      $('#card3').html(data.results[0].name).on('click', display)
+      $('#card4').html(data.results[8].name).on('click', display)
+      $('#card9').html(data.results[9].name).on('click', display)
+    })
+  })
 
-$.get("http://swapi.dev/api/people/?page=4", function(data) {
-  $('#card5').html(data.results[0].name);
-})
-$.get("http://swapi.dev/api/people/?page=5", function(data) {
-  $('#card6').html(data.results[9].name);
-  $('#card8').html(data.results[2].name);
-})
+           $.get("http://swapi.dev/api/people/?page=4", (data) => {
+           $('#card5').html(data.results[0].name).on('click', display)
+      })
+           $.get("http://swapi.dev/api/people/?page=5", (data) => {
+           $('#card6').html(data.results[9].name).on('click', display)
+           $('#card8').html(data.results[2].name).on('click', display)
+      })
 
-$.get("http://swapi.dev/api/people/?page=7", function(data) {
-  $('#card10').html(data.results[5].name);
-})
-$.get("http://swapi.dev/api/people/?page=8", function(data) {
-  $('#card11').html(data.results[7].name);
+           $.get("http://swapi.dev/api/people/?page=7", (data) => {
+           $('#card10').html(data.results[5].name).on('click', display)
+      })
+           $.get("http://swapi.dev/api/people/?page=8", (data) => {
+           $('#card11').html(data.results[7].name).on('click', display)
+      })
+  // })
 
-})
+  const display = () => {
+    const target = $(event.currentTarget).text()
+    $.get('https://swapi.dev/api/people/?search=' + target, data => {
+      const result = data.results[0]
+      $('#name').text(result.name)
+      $('#height').text('height: ' + result.height)
+      $('#mass').text('mass: ' + result.mass)
+      $('#birth').text('birth-year: ' + result.birth_year)
+    })
 
 
 
+    const $modal = $('#modal')
 
-const $modal = $('#modal')
-const $closeModal = $('#close')
-
-const openModal = () => {
-  $modal.css('display', 'block')
-  $('#card-name').text($(event.currentTarget).text())
-}
-
-const closeModal = () => {
-  $modal.css('display', 'none')
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-      closeModal();
+    const openModal = () => {
+      $modal.css('display','block')
     }
-  };
 
-// $('.card').on('click', openModal)
-$closeModal.on('click', closeModal)
-})
+    $('.card').on('click', openModal)
+
+    const closeModal = () => {
+      $modal.css('display', 'none')
+    }
+
+    const $closeModal = $('#close')
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+          closeModal();
+        }
+      };
+  }
+
+
+
+
+
+
+
+/* CODE GRAVEYARD
+
+
+
+
+
+
+
+
+console.log(data.results)
+//     for (let i = 0; i < data.results.length; i++) {
+//     let $name = data.results[i].name
+//     let $haircolor = data.results[i].height
+//     let $mass = data.results[i].mass
+//     let $birth = data.results[i].birth_year
+//     console.log(data.results[i])
+//
+//     const $modName = $('<p>')
+//     .text('name: ' + $name)
+//     .appendTo($('.modal-body'))
+//
+//     const $modHair = $('<p>')
+//     .text('haircolor: ' + $haircolor)
+//     .appendTo($('.modal-body'))
+// }
+
+// $('.card').on('click', event => {
+
+  // let getCard = $(event.target).attr('id')
+  // console.log(getCard);
+
+// const $modal = $('#modal')
+
+//
+// const openModal = () => {
+//   $modal.css('display', 'block')
+//   $('#card-name').text($(event.currentTarget).text())
+// }
+//
+
+//
+//
+//
+
+//   $closeModal.on('click', closeModal)
+//   })
+//
+//
+//
+$('.card').on('click', openModal)
+*/
