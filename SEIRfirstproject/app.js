6cfe7fff61e0192310236d0692a03ff9d230d27b
Keng Lee <KengLee@Hoeungs-MacBook-Pro.local>
2020-09-01 10:12:54 -0700

    changed background and card styles

--- SEIRfirstproject/app.js
+++ SEIRfirstproject/app.js
@@ -1,80 +1,67 @@
 $(() => {
 
+  let link = "https://swapi.dev/api/people/"
 
-
-    let link = `https://swapi.dev/api/people/?page=1`
-
-  $.ajax({
+  $.ajax ({
     url: link,
     type: 'GET',
-  }).then(
-    (data) => {
-      $('#card2').html(data.results[9].name).on('click', display)
-      $('#card7').html(data.results[3].name).on('click', display)
-      $('#card1').html(data.results[0].name).on('click', display)
-      }
-      )
-      $.get("https://swapi.dev/api/people/?page=2", (data) => {
-      $('#card3').html(data.results[0].name).on('click', display)
-      $('#card4').html(data.results[8].name).on('click', display)
-      $('#card9').html(data.results[9].name).on('click', display)
-    })
+    // linked API to app.js
+  }).then( data => {
+    $('#card1').html(data.results[0].name).on('click', stats)
+    $('#card2').html(data.results[9].name).on('click', stats)
+    $('#card7').html(data.results[3].name).on('click', stats)
+  })
+    $.get("https://swapi.dev/api/people/?page=2", (data) => {
+    $('#card3').html(data.results[0].name).on('click', stats)
+    $('#card4').html(data.results[8].name).on('click', stats)
+    $('#card9').html(data.results[9].name).on('click', stats)
+  })
+    $.get("https://swapi.dev/api/people/?page=4", (data) => {
+    $('#card5').html(data.results[0].name).on('click', stats)
+  })
+    $.get("https://swapi.dev/api/people/?page=5", (data) => {
+    $('#card6').html(data.results[9].name).on('click', stats)
+    $('#card8').html(data.results[2].name).on('click', stats)
+  })
+    $.get("https://swapi.dev/api/people/?page=7", (data) => {
+    $('#card10').html(data.results[5].name).on('click', stats)
+  })
+    $.get("https://swapi.dev/api/people/?page=8", (data) => {
+    $('#card11').html(data.results[7].name).on('click', stats)
   })
 
-           $.get("https://swapi.dev/api/people/?page=4", (data) => {
-           $('#card5').html(data.results[0].name).on('click', display)
-      })
-           $.get("https://swapi.dev/api/people/?page=5", (data) => {
-           $('#card6').html(data.results[9].name).on('click', display)
-           $('#card8').html(data.results[2].name).on('click', display)
-      })
 
-           $.get("https://swapi.dev/api/people/?page=7", (data) => {
-           $('#card10').html(data.results[5].name).on('click', display)
-      })
-           $.get("https://swapi.dev/api/people/?page=8", (data) => {
-           $('#card11').html(data.results[7].name).on('click', display)
-      })
-  // })
+// make a function that links the api data into card1 div
 
-  const display = () => {
-    const target = $(event.currentTarget).text()
-    $.get('https://swapi.dev/api/people/?search=' + target, data => {
-      const result = data.results[0]
-      $('#name').text(result.name)
-      $('#height').text('height: ' + result.height)
-      $('#mass').text('mass: ' + result.mass)
-      $('#birth').text('birth-year: ' + result.birth_year)
-    })
+const stats = () => {
+  // because I gave the data name thats what is going to return
+  const target = $(event.currentTarget).text()
+  console.log(target);
+  $.get("https://swapi.dev/api/people/?search=" + target, data => {
+    const results = data.results[0]
+    $('#name').text(results.name)
+    $('#height').text('height: ' + results.height)
+    $('#mass').text('mass: ' + results.mass)
+    $('#birth').text('birth: ' + results.birth_year)
+  })
+}
 
+// made the modal so when you click on a card it pulls up a modal
+const $modal = $('#modal')
+const $close = $('#close')
 
+const openModal = () => {
+  $modal.css('display', 'block')
+}
 
-    const $modal = $('#modal')
+const close = () => {
+  $modal.css('display', 'none')
+}
 
-    const openModal = () => {
-      $modal.css('display','block')
-    }
+$('.card').on('click', openModal)
+$('#close').on('click', close)
 
-    $('.card').on('click', openModal)
-
-    const closeModal = () => {
-      $modal.css('display', 'none')
-    }
-
-    const $closeModal = $('#close')
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-          closeModal();
-        }
-      };
-  }
-
-
-
-
-
-
+})
 
 /* CODE GRAVEYARD
 
@@ -126,4 +113,61 @@ console.log(data.results)
 //
 //
 $('.card').on('click', openModal)
+*/
+
+/*
+$(() => {
+
+
+
+    let link = `https://swapi.dev/api/people/?page=1`
+
+  $.ajax({
+    url: link,
+    type: 'GET',
+  }).then(
+    (data) => {
+
+
+
+
+
+
+
+  // })
+
+  const display = () => {
+    const target = $(event.currentTarget).text()
+    console.log(target);
+    $.get('https://swapi.dev/api/people/?search=' + target, data => {
+      const result = data.results[0]
+      $('#name').text(result.name)
+      $('#height').text('height: ' + result.height)
+      $('#mass').text('mass: ' + result.mass)
+      $('#birth').text('birth-year: ' + result.birth_year)
+    })
+
+
+
+    const $modal = $('#modal')
+
+    const openModal = () => {
+      $modal.css('display','block')
+    }
+
+    $('.card').on('click', openModal)
+
+    const closeModal = () => {
+      $modal.css('display', 'none')
+    }
+
+    const $closeModal = $('#close')
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+          closeModal();
+        }
+      };
+  }
+
 */
